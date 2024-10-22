2f015dea8619b898f060e16a8dad00ec92198d9a
Hridoy <foyzur.r.h@gmail.com>
2024-10-23 00:18:07 +0600

    PDO

--- index.php
+++ index.php
@@ -3,6 +3,17 @@
 
 require 'functions.php';
 
-require 'routers.php';
+$dsn = "mysql:host=localhost;port=3306;dbname=php;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
 
 
